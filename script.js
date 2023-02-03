926253967bfc970688ff112e193657084fd90467
Wanderson Gustavo da Silva <62720366+WandersonGustavo@users.noreply.github.com>
2023-02-03 19:52:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-var valorEmDollar = 28.89;
-var cotacaoDollar = 5.32;
-var valorEmReal = valorEmDollar * cotacaoDollar;
-valorEmReal = valorEmReal.toFixed(2);
-alert(valorEmReal + " R$");
+var bookW =  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwcHx3DFkNEXm89IRdaU4nOM5mLEkl1-gOg7vfBfPqLQa6P5E-Cm4p4rxMmJhXpIftonE&usqp=CAU'
+
+var listaFilms= [ ]
+
+listaFilms[0]= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwcHx3DFkNEXm89IRdaU4nOM5mLEkl1-gOg7vfBfPqLQa6P5E-Cm4p4rxMmJhXpIftonE&usqp=CAU'
+
+document.write('<img src = '+listaFilms[0] +'>');
+ 
+ 
+ 
+
