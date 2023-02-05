10446f53eb8b8e5e15245ce295ea45853dcc22a6
Wanderson Gustavo da Silva <62720366+WandersonGustavo@users.noreply.github.com>
2023-02-04 21:32:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var bookW =  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwcHx3DFkNEXm89IRdaU4nOM5mLEkl1-gOg7vfBfPqLQa6P5E-Cm4p4rxMmJhXpIftonE&usqp=CAU'
-
-var listaFilms= [ ]
-
-listaFilms[0]= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwcHx3DFkNEXm89IRdaU4nOM5mLEkl1-gOg7vfBfPqLQa6P5E-Cm4p4rxMmJhXpIftonE&usqp=CAU'
-
-document.write('<img src = '+listaFilms[0] +'>');
- 
- 
- 
-
+function adicionarFilme(){
+  var filmeFavorito= document.getElementById('filme').value
+ // document do filme alert(filmeFavorito)
+}
