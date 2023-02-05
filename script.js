0551952d8517fa3659e6dea070c97c72e2175701
Wanderson Gustavo da Silva <62720366+WandersonGustavo@users.noreply.github.com>
2023-02-05 20:01:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-function adicionarFilme(){
-  var filmeFavorito= document.getElementById('filme').value
- // document do filme alert(filmeFavorito)
+function adicionarVitoria() {}
+function adicionarDerrota() {}
+function adicionarEmpate() {}
+var rafaNome = "Rafa";
+var vitoriasRafa = 0;
+var empatesRafa;
+var paulo = {
+  nome: "Paulo",
+  vitoria: 0,
+  derrota: 0,
+  empate: 0,
+  pts: 0
+}; //objeto
+var rafa = {
+  nome: "Rafa",
+  vitoria: 0,
+  derrota: 0,
+  empate: 0,
+  pts: 0
+};
+//alert(paulo.vitoria);
+
+document.getElementById('Tabela Jogadores');
+elementoTabela.innerHTML = "
+  <tr>
+        <td>${paulo.nome}</td>
+        <td>${paulo.empate}</td>
+        <td>${paulo.derrota}</td>
+        <td>${paulo.ponto}</td>
+        <td>${paulo.vitoria}</td>
+        <td><button 
+"
+function adicionarVitoria(jogador){
+  jogador.vitoria++
+  jogador.pontos=jogador.pontos+3
+  console.log(jogador)
 }
+
+
+
+
+
+
+
+
